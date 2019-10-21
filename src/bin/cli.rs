c95b04b2ef83d77d7861a4456aab57642ef4460f
Yasushi Sakai <ysshski@gmail.com>
2019-10-20 21:02:31 -0400

    efficient line spacing

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -10,10 +10,8 @@ use liquid_demons::{create_matrix, calculate, poll_result, Setting, PollResult};
 use serde_json::{json, Map, Value};
 use std::io::prelude::*;
 use term;
-use term::StdoutTerminal;
 
 pub fn main () {
-
     let matches = App::new("liq")
         .version("0.0.0")
         .about("liquid demons")
@@ -50,7 +48,6 @@ pub fn main () {
     let result = poll_result(&settings.voters, &settings.policies, calulation_result);
 
     // printing the result
-
     if matches.is_present("json") {
 
         let mut out: Map<String, Value> = Map::new();
@@ -70,10 +67,101 @@ pub fn main () {
             pretty_print_settings(&settings).unwrap();
         }
 
-
-
         pretty_print_result(&result).expect("something went wron when printing");
     }
+}
+
+fn pretty_print_settings(setting: &Setting) -> Result<(), Box<dyn Error>> {
+
+    let mut t = term::stdout().unwrap();
+
+    let mut max_length = 0;
+
+    for v in &setting.voters {
+        if max_length < v.len() {
+            max_length = v.len();
+        }
+    }
+
+    for p in &setting.policies {
+        if max_length < p.len() {
+            max_length = p.len();
+        }
+    }
+
+    max_length = max_length;
+
+    t.attr(term::Attr::Bold)?;
+    write!(t, "{:10}", "title")?;
+    t.reset()?;
+
+    if let Some(title) = &setting.title {
+        write!(t, "{}\n", title)?;
+    }
+
+    t.attr(term::Attr::Bold)?;
+    write!(t, "{:10}", "policies")?;
+    t.reset()?;
+
+    t.fg(term::color::GREEN)?; 
+    for p in &setting.policies {
+        write!(t, "{:width$} ", p, width=&max_length)?;
+    }
+
+    t.reset()?;
+    write!(t, "\n")?;
+
+    t.attr(term::Attr::Bold)?;
+    write!(t, "{:10}","voters")?;
+    t.reset()?;
+
+    t.fg(term::color::BLUE)?; 
+    for v in &setting.voters {
+        write!(t, "{:width$} ", v, width=&max_length)?;
+    }
+    t.reset()?;
+
+    write!(t, "\n\n")?;
+
+    let votes: &Map<String, Value> = setting.votes.as_object().unwrap();
+
+    t.attr(term::Attr::Bold)?;
+    write!(t, "{:10}","votes")?;
+    t.reset()?;
+
+    println!(); 
+
+    for (from, vote_value) in Vec::from_iter(votes.iter()) {
+        t.fg(term::color::BLUE)?;
+        t.attr(term::Attr::Bold)?;
+        write!(t, "  {:width$}", from, width=&max_length)?;
+        t.reset()?;
+        write!(t, " → ")?;
+        let vote: &Map<String, Value> = vote_value.as_object().unwrap();
+
+        for policy in &setting.policies {
+            if let Some(value) = vote.get(policy) {
+                t.fg(term::color::GREEN)?;
+                write!(t, "{:width$}: ", policy, width=&max_length)?;
+                t.reset()?;
+                write!(t, "{:width$}, ", value, width=&max_length)?;
+            }
+        }
+
+        for voter in &setting.voters {
+            if let Some(value) = vote.get(voter) {
+                t.fg(term::color::BLUE)?;
+                write!(t, "{:width$}: ", voter, width=&max_length)?;
+                t.reset()?;
+                write!(t, "{:width$}, ", value, width=&max_length)?;
+            }
+        }
+
+        write!(t, "\n")?;
+    }
+
+    write!(t, "\n")?;
+    Ok(())
 }
 
 fn pretty_print_result(result: &PollResult) -> Result<(), Box<dyn Error>> {
@@ -85,6 +173,19 @@ fn pretty_print_result(result: &PollResult) -> Result<(), Box<dyn Error>> {
     votes.sort_by(|&(_, a), &(_, b)| b.partial_cmp(&a).unwrap_or(Ordering::Equal));
     let mut influence = Vec::from_iter(&result.influence);
     influence.sort_by(|&(_, a), &(_, b)| b.partial_cmp(&a).unwrap_or(Ordering::Equal));
+    let mut max_length = 0;
+
+    for (v, _) in &votes {
+        if max_length < v.len()  {
+            max_length = v.len()
+        }   
+    } 
+
+    for (i, _) in &influence {
+        if max_length < i.len() {
+            max_length = i.len()
+        }
+    }
 
     t.attr(term::Attr::Bold)?;
     write!(t, "result\n  policies:\n")?;
@@ -97,7 +198,7 @@ fn pretty_print_result(result: &PollResult) -> Result<(), Box<dyn Error>> {
     for (p, v) in &votes {
         t.attr(term::Attr::Bold)?;
         t.fg(term::color::GREEN)?;
-        write!(t, "  {:10} ", p)?;
+        write!(t, "  {:width$} ", p, width=&max_length)?;
         t.reset()?;
         write!(t, "{:.4}\n", v)?;
     }
@@ -114,96 +215,16 @@ fn pretty_print_result(result: &PollResult) -> Result<(), Box<dyn Error>> {
     for (inf, v) in &influence {
         t.attr(term::Attr::Bold)?;
         t.fg(term::color::BLUE)?;
-        write!(t, "  {:10} ", inf)?;
+        write!(t, "  {:width$} ", inf, width=&max_length)?;
         t.reset()?;
         write!(t, "{:.4}\n", v)?;
     }
-
-    Ok(())
-}
-
-fn pretty_print_settings(setting: &Setting) -> Result<(), Box<dyn Error>> {
-
-    let mut t = term::stdout().unwrap();
-
-    t.attr(term::Attr::Bold)?;
-    write!(t, "{:10}", "title ")?;
-    t.reset()?;
-
-    if let Some(title) = &setting.title {
-        write!(t, "{}\n", title)?;
-    }
-
-    t.attr(term::Attr::Bold)?;
-    write!(t, "{:10}", "policies")?;
-    t.reset()?;
-
-    t.fg(term::color::GREEN)?; 
-    for p in &setting.policies {
-        write!(t, "{:20} ", p)?;
-    }
-
-    t.reset()?;
-    write!(t, "\n")?;
-
-    t.attr(term::Attr::Bold)?;
-    write!(t, "{:10}","voters")?;
-    t.reset()?;
-
-    t.fg(term::color::BLUE)?; 
-    for v in &setting.voters {
-        write!(t, "{:20} ", v)?;
-    }
-    t.reset()?;
-
-    write!(t, "\n\n")?;
-
-    let votes: &Map<String, Value> = setting.votes.as_object().unwrap();
-
-    t.attr(term::Attr::Bold)?;
-    write!(t, "{}","votes")?;
-    t.reset()?;
-
-    println!(); 
-
-    for (from, vote_value) in Vec::from_iter(votes.iter()) {
-        t.fg(term::color::BLUE)?;
-        t.attr(term::Attr::Bold)?;
-        write!(t, "  {:10}", from)?;
-        t.reset()?;
-        write!(t, "→ ")?;
-        let vote: &Map<String, Value> = vote_value.as_object().unwrap();
-
-        for policy in &setting.policies {
-            if let Some(value) = vote.get(policy) {
-                t.fg(term::color::GREEN)?;
-                write!(t, "{:10}: ", policy)?;
-                t.reset()?;
-                write!(t, "{:10}, ", value)?;
-            }
-        }
-
-        for voter in &setting.voters {
-            if let Some(value) = vote.get(voter) {
-                t.fg(term::color::BLUE)?;
-                write!(t, "{:10}: ", voter)?;
-                t.reset()?;
-                write!(t, "{:10}, ", value)?;
-            }
-        }
-
-        write!(t, "\n")?;
-    }
-
-    write!(t, "\n")?;
     Ok(())
 }
 
 fn read_settings_from_file(path: &Path) -> Result<Setting, Box<dyn Error>> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
-
     let settings: Setting = serde_json::from_reader(reader)?;
-
     Ok(settings)
 }
